687def49544c5e059803a9e973125dfa1281ac1d
KongchengPro <KongchengPro@163.com>
2021-03-23 20:50:04 +0800

    重构
    将common.go移动至pkg文件夹

--- internal/app/opqbot/client.go
+++ internal/app/opqbot/client.go
@@ -4,6 +4,7 @@ import (
 	"HappyOPQ/internal/app/common"
 	"HappyOPQ/internal/app/common/retry"
 	"HappyOPQ/internal/app/config"
+	common2 "HappyOPQ/pkg/common"
 	"HappyOPQ/pkg/log"
 	sio "github.com/graarh/golang-socketio"
 	"github.com/graarh/golang-socketio/transport"
@@ -53,37 +54,37 @@ func (c *Communicator) connect() error {
 	return nil
 }
 func (c *Communicator) init() {
-	common.Must(c.client.On(sio.OnConnection, func(_c *sio.Channel) {
+	common2.Must(c.client.On(sio.OnConnection, func(_c *sio.Channel) {
 		log.Info("成功与 OPQBot 连接，ID 为", c.client.Id())
 		c.FlagChan <- ConnectionSucceed
 	}))
 
-	common.Must(c.client.On(sio.OnDisconnection, func(_c *sio.Channel) {
+	common2.Must(c.client.On(sio.OnDisconnection, func(_c *sio.Channel) {
 		log.Error("与 OPQBot 断开连接")
 		c.FlagChan <- ConnectionTerminate
 	}))
 
-	common.Must(c.client.On(sio.OnError, func(_c *sio.Channel) {
+	common2.Must(c.client.On(sio.OnError, func(_c *sio.Channel) {
 		log.Error("与 OPQBot 的连接发生错误")
 		c.FlagChan <- ConnectionTerminate
 	}))
 
-	common.Must(c.client.On(OnFriendMsgs, func(conn *sio.Channel, msg FriendMessage) {
+	common2.Must(c.client.On(OnFriendMsgs, func(conn *sio.Channel, msg FriendMessage) {
 		log.InfoF("收到 OnFriendMsgs 事件：%+v", msg)
 		c.EventChan <- &msg
 	}))
 
-	common.Must(c.client.On(OnGroupMsgs, func(c *sio.Channel, msg interface{}) {
+	common2.Must(c.client.On(OnGroupMsgs, func(c *sio.Channel, msg interface{}) {
 		log.InfoF("收到 OnGroupMsgs 事件：%+v", msg)
 	}))
 
-	common.Must(c.client.On(OnEvents, func(conn *sio.Channel, msg interface{}) {
+	common2.Must(c.client.On(OnEvents, func(conn *sio.Channel, msg interface{}) {
 		log.InfoF("收到 OnEvents 事件：%+v", msg)
 	}))
 }
 
 func (c *Communicator) Init() {
-	common.Must(c.connect())
+	common2.Must(c.connect())
 	c.init()
 	go c.recover()
 }
@@ -93,7 +94,7 @@ func (c *Communicator) recover() {
 		f := <-c.FlagChan
 		switch f {
 		case ConnectionTerminate:
-			common.Must(c.retry.On(c.connect))
+			common2.Must(c.retry.On(c.connect))
 			c.init()
 		}
 	}
--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,7 +1,5 @@
 package common
 
-
-
 func Must(err error) {
 	if err != nil {
 		panic(err)
